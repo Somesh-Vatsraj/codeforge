461e8a42c1015819c41843abb1f62b1a302bba68
Somesh Vatsraj <someshsoftwareengineer@gmail.com>
2026-09-17 19:30:22 +0530

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App.jsx';
+import { AuthProvider, SettingsProvider, ThemeProvider } from './store.jsx';
+import './styles/index.css';
+import './styles/admin.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <ThemeProvider>
+        <SettingsProvider>
+          <AuthProvider>
+            <App />
+          </AuthProvider>
+        </SettingsProvider>
+      </ThemeProvider>
+    </BrowserRouter>
+  </React.StrictMode>,
+);
